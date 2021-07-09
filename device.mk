07f5ae1a0dd081a2d0f02fa462791e84be0f7b4b
Rizky Benggolo <travarilo@gmail.com>
2021-07-09 12:57:30 +0000

    X00T: Reenable support for Touch HIDL

--- device.mk
+++ device.mk
@@ -452,6 +452,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libtinyxml2
 
+# Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.X00T
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
